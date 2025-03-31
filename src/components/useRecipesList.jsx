d7b47a562ae3ac383365f25d5593b751df71a704
AlexDuminica <alexandru.duminica98@gmail.com>
2025-03-31 11:42:48 +0100

    Formatting useRecipeList

--- src/components/useRecipesList.jsx
+++ src/components/useRecipesList.jsx
@@ -1,30 +1,26 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { APIs } from "../api/endpoint";
-
 
 // convert thids into a hook and get the data from the hook and use it in your component
 export const useRecipeList = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
-    const[recipes, setRecipes] = useState([]);
-    const[isLoading, setIsLoading] = useState(false);
-    const[error, setError] = useState();
-    
-        useEffect(() => {
-        setIsLoading(true)
-        fetch(APIs.search)
-            .then((response) => response.json())
-            .then((data) => {
-                setRecipes(data.meals)
-            })
-            .catch((e)=>{
-                setError(e)
-            })
-            .finally(()=> {
-                setIsLoading(false)
-            })
-        }, [])
-    
-        
-        return  {recipes,isLoading,error};
-      };
-      
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(APIs.search)
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipes(data.meals);
+      })
+      .catch((e) => {
+        setError(e);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return { recipes, isLoading, error };
+};
